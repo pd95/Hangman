e484f9d9a5460a78a5f613b15fddf9632a5fc103
Philipp <pd95@users.noreply.github.com>
2022-04-24 20:30:23 +0200

    Initial Commit

---
+++ Hangman/HangmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  HangmanApp.swift
+//  Hangman
+//
+//  Created by Philipp on 24.04.22.
+//
+
+import SwiftUI
+
+@main
+struct HangmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
