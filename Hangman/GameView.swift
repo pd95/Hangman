c5ceffc16eb414f65f9f6c00cdcce201c4b3ebb7
Philipp <pd95@users.noreply.github.com>
2022-04-25 18:14:00 +0200

    Removing non-functional transitions.

--- Hangman/GameView.swift
+++ Hangman/GameView.swift
@@ -13,15 +13,12 @@ struct GameView: View {
     var body: some View {
         if case .readyToStart = state {
             StartScreen(state: $state)
-                .transition(.asymmetric(insertion: .opacity, removal: .move(edge: .leading)))
         }
         else if case .running = state {
             PlayingScreen(state: $state)
-                .transition(.asymmetric(insertion: .move(edge: .trailing), removal: .move(edge: .leading)))
         }
         else {
             EndScreen(state: $state)
-                .transition(.asymmetric(insertion: .move(edge: .trailing), removal: .move(edge: .top)))
         }
     }
 }
